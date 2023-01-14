b75e8b55e1459e7b6f0603c9c7d8222fea0604a8
Samosadov <for-spam2@yandex.ru>
2023-01-14 11:56:06 +0300

    Cat example

--- src/ru_synergy/Main.java
+++ src/ru_synergy/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Cat[] cats = new Cat[3];
+        cats[0] = new Cat("Barsik");
+        cats[1] = new Cat("Behemot");
+        cats[2] = new Cat("Murzik");
 
+        for (Cat cat : cats) System.out.println(cat);
     }
 }
